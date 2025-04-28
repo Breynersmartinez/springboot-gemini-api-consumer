c7b54b18b1133da38ffac47e3b04712309429fe2
Breiner Saul Martinez Muñoz <153401516+Breynersmartinez@users.noreply.github.com>
2025-04-28 12:07:30 -0500

    Add files via upload

---
+++ src/main/java/com/example/springboot_gemini_api_consumer/SpringbootGeminiApiConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_gemini_api_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGeminiApiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGeminiApiConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_gemini_api_consumer/SpringbootGeminiApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_gemini_api_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGeminiApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
